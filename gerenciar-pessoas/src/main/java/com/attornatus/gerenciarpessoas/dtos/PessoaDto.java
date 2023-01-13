ebf86868fb646980d380c35eca86876007461ab4
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-01-13 19:59:31 -0300

    Criado os Dtos

---
+++ gerenciar-pessoas/src/main/java/com/attornatus/gerenciarpessoas/dtos/PessoaDto.java
@@ -0,0 +1,26 @@
+package com.attornatus.gerenciarpessoas.dtos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+public class PessoaDto implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Date dataNascimento;
+}
+
