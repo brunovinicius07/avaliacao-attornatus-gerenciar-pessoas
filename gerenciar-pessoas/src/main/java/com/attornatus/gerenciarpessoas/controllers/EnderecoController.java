cf95e1a6b909754e3687b0346cfd2b17da0e2b8c
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-01-15 12:16:09 -0300

    Criado o método Buscar Endereço

--- gerenciar-pessoas/src/main/java/com/attornatus/gerenciarpessoas/controllers/EnderecoController.java
+++ gerenciar-pessoas/src/main/java/com/attornatus/gerenciarpessoas/controllers/EnderecoController.java
@@ -2,6 +2,7 @@ package com.attornatus.gerenciarpessoas.controllers;
 
 import com.attornatus.gerenciarpessoas.dtos.EnderecoDto;
 import com.attornatus.gerenciarpessoas.entities.Endereco;
+import com.attornatus.gerenciarpessoas.entities.Pessoa;
 import com.attornatus.gerenciarpessoas.repositories.EnderecoRepository;
 import com.attornatus.gerenciarpessoas.repositories.PessoaRepository;
 import com.attornatus.gerenciarpessoas.services.EnderecoService;
@@ -9,10 +10,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/endereco")
@@ -31,4 +31,14 @@ public class EnderecoController {
     public ResponseEntity<Endereco> cadastrarEndereco(@RequestBody @Valid EnderecoDto enderecoDto){
         return new ResponseEntity<Endereco>(enderecoService.cadastrarEndereco(enderecoDto), HttpStatus.CREATED);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> buscarEndereco(@PathVariable(value = "id") Long id){
+        Optional<Endereco> endereco = enderecoService.buscarEndereco(id);
+        if (!endereco.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Endereço não encontrado");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(endereco.get());
+    }
+
 }
