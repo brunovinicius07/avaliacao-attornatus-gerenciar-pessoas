db3d88f03d3de0ee4c15a32b34b934bf2e3c291d
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-01-15 10:10:24 -0300

    Refatorado PessoaController

--- gerenciar-pessoas/src/main/java/com/attornatus/gerenciarpessoas/controllers/PessoaController.java
+++ gerenciar-pessoas/src/main/java/com/attornatus/gerenciarpessoas/controllers/PessoaController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 
 
 @RestController
@@ -25,6 +26,15 @@ public class PessoaController {
     @PostMapping
     public ResponseEntity<Pessoa> cadastrarPessoa(@RequestBody @Valid PessoaDto pessoaDto){
         return new ResponseEntity<Pessoa>(pessoaService.cadastrarPessoa(pessoaDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> buscarPessoa(@PathVariable(value = "id") Long id){
+        Optional<Pessoa> pessoa = pessoaService.buscarPessoa(id);
+        if (!pessoa.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pessoa não encontrada");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(pessoa.get());
     }
 
 
