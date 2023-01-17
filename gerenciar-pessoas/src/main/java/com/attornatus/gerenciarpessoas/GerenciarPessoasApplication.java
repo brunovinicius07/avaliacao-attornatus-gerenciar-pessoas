54af99c51c541c84f5d87da04826b6544c324935
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-01-17 12:23:56 -0300

    removido imports desnecessarios

--- gerenciar-pessoas/src/main/java/com/attornatus/gerenciarpessoas/GerenciarPessoasApplication.java
+++ gerenciar-pessoas/src/main/java/com/attornatus/gerenciarpessoas/GerenciarPessoasApplication.java
@@ -2,7 +2,6 @@ package com.attornatus.gerenciarpessoas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,10 +10,5 @@ public class GerenciarPessoasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciarPessoasApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String index(){
-		return "Olá Mundo!";
 	}
 }
